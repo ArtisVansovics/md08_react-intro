bb3d40ddf23d0554e69aefc9f178d85821076b2b
Artis Vansovics <vansovics.artis@gmail.com>
2022-04-22 00:37:21 +0300

    Added image frames

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import Header from './components/header/header';
 import { Button, ButtonRed } from './components/button/button';
+import ImgFrame from './components/img-frame/img-frame';
 
 const App = () => (
   <div className="App">
@@ -15,6 +16,20 @@ const App = () => (
       <ButtonRed text="Button 1" />
       <ButtonRed text="Button 2" />
       <ButtonRed text="Button 3" />
+    </div>
+    <div className="App__container">
+      <ImgFrame
+        src="https://png.pngtree.com/element_our/png/20181009/thai-cat-cream-tabby-sitting-png_131622.jpg"
+      />
+      <ImgFrame
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6RTvGVK-qPzm36I20BcCtPl-XARIzGfblxA&usqp=CAU"
+      />
+      <ImgFrame
+        src="https://www.pngfind.com/pngs/m/311-3116406_cat-png-free-download-cute-cat-png-transparent.png"
+      />
+      <ImgFrame
+        src="https://i.pinimg.com/564x/10/d9/63/10d9631abb9778072c1da264a6d4709a--cats.jpg"
+      />
     </div>
   </div>
 );
--- src/components/img-frame/img-frame.tsx
+++ src/components/img-frame/img-frame.tsx
@@ -1,1 +1,13 @@
 import './img-frame.scss';
+
+type ImgFrameProps = {
+  src: string
+}
+
+const ImgFrame = ({ src }: ImgFrameProps) => (
+  <div className="ImgFrame">
+    <img src={src} alt="" className="ImgFrame__image" />
+  </div>
+);
+
+export default ImgFrame;
